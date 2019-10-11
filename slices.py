6f4f7d286f219318146dea5d4e4fb681cf8a66ae
Anthony Lyappiev <archlinux@antavr.ru>
2019-10-11 13:26:24 +0300

    Update slices.py

--- slices.py
+++ slices.py
@@ -11,14 +11,14 @@ Slice = Tuple[Union[int, slice], ...]
 
 def get_slice_masks(shape: Tuple[int, ...], ndim: int) -> Generator[Mask, Any, None]:
     _ndim = len(shape)
-    assert 0 <= ndim <= _ndim
+    # assert 0 <= ndim <= _ndim
     for _comb in itertools.combinations(range(_ndim - 1, -1, -1), ndim):  # type: Tuple[int, ...]
         yield tuple(slice(shape[n]) if n in _comb else range(shape[n]) for n in range(_ndim))
 
 
 def gen_slices(slice_mask: Mask, tmp_slice: Slice = tuple(), ndim: int = 0) -> Generator[Slice, Any, None]:
     _ndim = len(slice_mask)
-    assert 0 <= ndim <= _ndim + 1
+    # assert 0 <= ndim <= _ndim + 1
     if ndim >= _ndim:
         yield tmp_slice
         return
